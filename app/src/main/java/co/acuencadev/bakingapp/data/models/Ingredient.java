4d63554e199a18d9dd01712fa848ee66184286f4
Amador Cuenca <sphi02ac@gmail.com>
2018-11-02 08:55:32 -0500

    feat: Add network models

---
+++ app/src/main/java/co/acuencadev/bakingapp/data/models/Ingredient.java
@@ -0,0 +1,59 @@
+package co.acuencadev.bakingapp.data.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient {
+
+    private Integer id;
+
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public Ingredient(Integer id, Integer quantity, String measure, String ingredient) {
+        this.id = id;
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+}
