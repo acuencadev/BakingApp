4484396710f6a3c249296c3318780bcca4adedac
Amador Cuenca <sphi02ac@gmail.com>
2018-11-03 20:35:35 -0500

    feat: Initialize Databinding in the MainActivity

--- app/src/main/java/co/acuencadev/bakingapp/ui/main/MainActivity.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/main/MainActivity.java
@@ -1,15 +1,21 @@
 package co.acuencadev.bakingapp.ui.main;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import co.acuencadev.bakingapp.R;
+import co.acuencadev.bakingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
