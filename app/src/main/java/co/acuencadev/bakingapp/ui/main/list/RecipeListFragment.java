d80c08d66200e603a0a3477be17d61aa1442b805
Amador Cuenca <sphi02ac@gmail.com>
2018-11-05 11:00:30 -0500

    feat: Initialize the ViewModel into the RecipeListFragment

--- app/src/main/java/co/acuencadev/bakingapp/ui/main/list/RecipeListFragment.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/main/list/RecipeListFragment.java
@@ -1,19 +1,23 @@
 package co.acuencadev.bakingapp.ui.main.list;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import co.acuencadev.bakingapp.R;
+import co.acuencadev.bakingapp.utilities.InjectorUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecipeListFragment extends Fragment {
 
+    private RecipeListViewModel mViewModel;
 
     public RecipeListFragment() {
         // Required empty public constructor
@@ -27,4 +31,16 @@ public class RecipeListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_recipe_list, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        observeRecipeData();
+    }
+
+    private void observeRecipeData() {
+        RecipeListViewModelFactory factory = InjectorUtils.provideRecipeListViewModelFactory(
+                getActivity());
+        mViewModel = ViewModelProviders.of(this, factory).get(RecipeListViewModel.class);
+    }
 }
