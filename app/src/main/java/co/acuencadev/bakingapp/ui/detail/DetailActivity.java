7bb5789b38b41b18031bda75c733e89f1cdea3c3
Amador Cuenca <sphi02ac@gmail.com>
2018-11-05 15:49:08 -0500

    feat: Extract the Recipe ID from the intent

--- app/src/main/java/co/acuencadev/bakingapp/ui/detail/DetailActivity.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/detail/DetailActivity.java
@@ -1,15 +1,27 @@
 package co.acuencadev.bakingapp.ui.detail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import co.acuencadev.bakingapp.R;
+import co.acuencadev.bakingapp.ui.main.MainActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private int mRecipeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        getRecipeIdFromIntent();
+    }
+
+    private void getRecipeIdFromIntent() {
+        Intent intent = getIntent();
+
+        mRecipeId = intent.getIntExtra(MainActivity.EXTRA_RECIPE_ID, 0);
     }
 }
