fcb3b8278206eb571700196786bb3938bb4cc723
Amador Cuenca <sphi02ac@gmail.com>
2018-11-05 09:55:26 -0500

    feat: Display options menu in the MainActivity

--- app/src/main/java/co/acuencadev/bakingapp/ui/main/MainActivity.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/main/MainActivity.java
@@ -3,6 +3,9 @@ package co.acuencadev.bakingapp.ui.main;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import co.acuencadev.bakingapp.R;
 import co.acuencadev.bakingapp.databinding.ActivityMainBinding;
@@ -17,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
