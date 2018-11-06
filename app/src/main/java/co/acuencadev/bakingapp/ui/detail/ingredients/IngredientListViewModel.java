63950c3f60226dd62fe1011713bfb2c88a464313
Amador Cuenca <sphi02ac@gmail.com>
2018-11-06 12:12:37 -0500

    refactor: Change the ingredient list view model data type

--- app/src/main/java/co/acuencadev/bakingapp/ui/detail/ingredients/IngredientListViewModel.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/detail/ingredients/IngredientListViewModel.java
@@ -1,25 +1,24 @@
 package co.acuencadev.bakingapp.ui.detail.ingredients;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import java.util.List;
 
 import co.acuencadev.bakingapp.data.BakingAppRepository;
-import co.acuencadev.bakingapp.data.models.Ingredient;
+import co.acuencadev.bakingapp.data.models.Recipe;
 
 public class IngredientListViewModel extends ViewModel {
 
     private final BakingAppRepository mRepository;
-    private final LiveData<List<Ingredient>> mIngredients;
+    private final LiveData<List<Recipe>> mRecipes;
 
     public IngredientListViewModel(BakingAppRepository repository) {
         this.mRepository = repository;
-        this.mIngredients = new MutableLiveData<>();
+        this.mRecipes = this.mRepository.getRecipes();
     }
 
-    public LiveData<List<Ingredient>> getIngredients() {
-        return this.mIngredients;
+    public LiveData<List<Recipe>> getRecipes() {
+        return this.mRecipes;
     }
 }
