e606425ca993e0b0a09ce7f856c3a7cc9574d6e3
Amador Cuenca <sphi02ac@gmail.com>
2018-11-06 12:07:36 -0500

    feat: Initialize view model in the ingredients list fragment

--- app/src/main/java/co/acuencadev/bakingapp/ui/detail/ingredients/IngredientListFragment.java
+++ app/src/main/java/co/acuencadev/bakingapp/ui/detail/ingredients/IngredientListFragment.java
@@ -1,8 +1,10 @@
 package co.acuencadev.bakingapp.ui.detail.ingredients;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +12,14 @@ import android.view.ViewGroup;
 
 import co.acuencadev.bakingapp.R;
 import co.acuencadev.bakingapp.databinding.FragmentIngredientListBinding;
+import co.acuencadev.bakingapp.utilities.InjectorUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IngredientListFragment extends Fragment {
 
+    private IngredientListViewModel mViewModel;
     private FragmentIngredientListBinding mBinding;
 
     public IngredientListFragment() {
@@ -31,4 +35,17 @@ public class IngredientListFragment extends Fragment {
         return mBinding.getRoot();
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        observeIngredientData();
+    }
+
+    private void observeIngredientData() {
+        IngredientListViewModelFactory factory = InjectorUtils.provideIngredientListViewModelFactory(
+                getActivity()
+        );
+        mViewModel = ViewModelProviders.of(this, factory).get(IngredientListViewModel.class);
+    }
 }
